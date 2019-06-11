0ed61f12a1252e69046cefe9a3e16c674581c097
blizzy <34592028+blizma@users.noreply.github.com>
2019-06-11 11:35:49 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,68 @@
+import setuptools
+
+VERSION = "0.0.1"
+TAGS = [
+    "thonkdb",
+    "database",
+    "python",
+    "utilities",
+    "enhancements"
+]
+CLASSIFIERS = [
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3 :: Only",
+    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: Implementation :: PyPy",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: Microsoft",
+    "Operating System :: Microsoft :: Windows :: Windows 10",
+    "Operating System :: Microsoft :: Windows :: Windows 8",
+    "Operating System :: Microsoft :: Windows :: Windows 8.1",
+    "Operating System :: Microsoft :: Windows :: Windows 7",
+    "Operating System :: MacOS",
+    "Operating System :: MacOS :: MacOS X",
+    "Operating System :: POSIX :: Linux",
+    "Operating System :: Other OS",
+    "Intended Audience :: Developers",
+    "Intended Audience :: End Users/Desktop",
+    "Topic :: Software Development :: Libraries",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    "Topic :: Utilities",
+    "Topic :: System",
+    "Topic :: Terminals",
+    "Development Status :: 5 - Production/Stable",
+    "Framework :: IDLE",
+    "Natural Language :: English",
+]
+MainURL = "https://github.com/blizma/thonkdb"
+URLs = 
+    {
+        "Bug Tracker": "https://github.com/blizma/thonkdb//issues",
+        "Source Code": "https://github.com/blizma/thonkdb/",
+        "License": "https://github.com/blizma/thonkdb/blob/master/LICENSE",
+    }
+setuptools.setup(
+    name="thonkdb",
+    version=VERSION,
+    author="Blizma",
+    author_email="me@blizma.us",
+    description="Storing data in Python, easily.",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url=MainURL,
+    license="MIT",
+    packages=setuptools.find_packages(),
+    classifiers=CLASSIFIERS,
+    project_urls=URLs,
+    keywords=TAGS,
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "area4>=2.7.0"
+    ]
+)
